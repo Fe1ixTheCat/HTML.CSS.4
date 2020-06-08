3829775110de55bab90b95a2eebeb86fa75f4e7a
Aleksandr <52758025+Fe1ixTheCat@users.noreply.github.com>
2020-06-09 00:40:41 +0300

    1
    1

---
+++ script.js
@@ -0,0 +1,13 @@
+var search = document.getElementById('search');
+var check = false;
+var input = document.getElementById('searchInput');
+
+function searchFunc() {
+  searchInput.style.display = "block";
+  check = true;
+}
+
+function closeAll() {
+
+  
+}
